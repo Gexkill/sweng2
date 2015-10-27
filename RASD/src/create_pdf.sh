ee3c7093ea2674b2e986d631f7e132efd3a530aa
tux_mind <massimo.dragano@gmail.com>
2015-10-27 14:23:23 +0100

    remove raw GET parameter for generating the pdf with pandoc

--- RASD/src/create_pdf.sh
+++ RASD/src/create_pdf.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sed -E 's,^\s*\[//\]: # \(pagebreak\).*$,\\pagebreak,' < index.md | pandoc -f markdown -o ../RASD.pdf
+sed -E 's,^\s*\[//\]: # \(pagebreak\).*$,\\pagebreak,;s/\?raw=true//g' < index.md | pandoc -f markdown -o ../RASD.pdf
